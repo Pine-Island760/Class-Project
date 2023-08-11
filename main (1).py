5176957ced761b359f25fac615d5e1e7eb776a28
Pine-Island760 <140592387+Pine-Island760@users.noreply.github.com>
2023-08-11 23:48:52 +0000

    actual project

--- main (1).py
+++ main (1).py
@@ -1,50 +1,27 @@
-# This is a header for the application
-# You should read this header and insert your name and your date below as part of the peer review
-# This is a typical part of any program
-# Author: <Jeff Sanders>
-# Creation Date: <24,Jul,23>
-# Below is a simple program with several.  You need to identify the issues and correct them.
+import json, requests
+ 
+base_url =  "https://api.openweathermap.org/data/2.5/weather"
+appid = "0a04a18985b95bbdd90429f6968d49f0"
+city = input(f"Please type in your city:")
+url = f"{base_url}?q={city}&units=imperial&APPID={appid}"
+print(url)
+print ()
 
-import random
-import time
+response = requests.get(url)
+unformatted_data = response.json()
 
-def displayIntro():
-	print('''You are in a land full of dragons. In front of you,
-	you see two caves. In one cave, the dragon is friendly
-	and will share his treasure with you. The other dragon
-	is greedy and hungry, and will eat you on sight.''')
-	print()
+print(unformatted_data)
 
-def chooseCave():
-  cave = '1'
-  while cave != '1' and cave != '2':
-    print('Which cave will you go into? (1 or 2)')
-    cave = input()
-  else:
-    return cave
+#unformatted temp
+temp = unformatted_data["main"]["temp"]
+print(f"The current temp is: {temp}")
 
-def checkCave(choseCave):
-    print('You approach the cave...')
-    #sleep for 2 seconds
-    time.sleep(2)
-    print('It is dark and spooky...')
-    #sleep for 2 seconds
-    time.sleep(2)
-    print('A large dragon jumps out in front of you! He opens his jaws and...')
-    print()
-    #sleep for 2 seconds
-    time.sleep(2)
-    friendlyCave = random.randint(1, 2)
+temp_max = unformatted_data["main"]["temp_max"]
+print(f"The max temp is: {temp_max}")
 
-    if 'choseCave' == str(friendlyCave):
-        print('Gives you his treasure!')
-    else:
-        print ('Gobbles you down in one bite!')
-
-
-
-displayIntro()
-caveNumber = input
-checkCave(caveNumber)
-    
-print("Thanks for playing")
+if city != quit:
+  
+else:
+  break
+  print()
+#need hel
